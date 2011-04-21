6533285fc59edbb65f0ede6c5a35aedb2daf4acc
Stefan Fussenegger <stf+git@molindo.at>
2011-04-21 18:49:57 +0200

    added EnumTests (green lights)

---
+++ src/test/java/org/compass/annotations/test/converter/enum1/B.java
@@ -0,0 +1,11 @@
+package org.compass.annotations.test.converter.enum1;
+
+/**
+ * @author kimchy
+ */
+public interface B {
+
+   public enum Type {
+        Note, Call, Meeting, Organisation, Attachment
+    }
+}
