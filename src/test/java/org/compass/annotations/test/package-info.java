3226f451e2532080cb19be4659583a1b2d1e85c8
Stefan Fussenegger <stf+git@molindo.at>
2011-04-22 14:18:36 +0200

    documented removed test cases

---
+++ src/test/java/org/compass/annotations/test/package-info.java
@@ -0,0 +1,12 @@
+package org.compass.annotations.test;
+
+/**
+ * ignored tests: 
+ * <ul>
+ * <li>org/compass/annotations/test/dynamic/jexl/JexlDynamicTests.java</li>
+ * <li>org/compass/annotations/test/dynamicproperty/map/simple/SimpleMapDynamicPropertyTests.java</li>
+ * <li>org/compass/annotations/test/dynamicproperty/simple/SimpleDynamicPropertyTests.java</li>
+ * <li>org/compass/annotations/test/spellcheck/SpellCheckAnnotationTests.java</li>
+ * <li>org/compass/annotations/test/subindexhash/SubIndexHashTests.java</li>
+ * </ul>
+ */
