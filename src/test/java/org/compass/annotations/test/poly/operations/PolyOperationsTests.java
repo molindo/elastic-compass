b0fbbda79e75f04b3dcd740e77e6b81d0d5e2cd0
Stefan Fussenegger <stf+git@molindo.at>
2011-04-22 10:57:39 +0200

    added PolyOperationsTests, all fail due to lack of polymorphism support

---
+++ src/test/java/org/compass/annotations/test/poly/operations/PolyOperationsTests.java
@@ -0,0 +1,125 @@
+package org.compass.annotations.test.poly.operations;
+
+import org.compass.annotations.test.AbstractAnnotationsTestCase;
+import org.compass.core.CompassSession;
+import org.compass.core.config.CompassConfiguration;
+import org.junit.Ignore;
+
+/**
+ * @author kimchy
+ */
+public class PolyOperationsTests extends AbstractAnnotationsTestCase {
+
+    protected void addExtraConf(CompassConfiguration conf) {
+        conf.addClass(Contract.class).addClass(A.class).addClass(B.class);
+    }
+
+    @Ignore("polymorphism not yet supported")
+    public void testGetPolyOperations() {
+        CompassSession session = openSession();
+
+        A a = new A();
+        a.id = 1;
+        session.save(a);
+
+        B b = new B();
+        b.id = 2;
+        session.save(b);
+
+        refresh(session);
+        
+        a = session.get(A.class, 1);
+        assertNotNull(a);
+
+        a = session.get(B.class, 1);
+        assertNull(a);
+
+        a = session.get(B.class, 2);
+        assertNotNull(a);
+
+        a = session.get(A.class, 2);
+        assertNotNull(a);
+
+        Contract c = session.get(Contract.class, 2);
+        assertNotNull(c);
+
+        c = session.get(Contract.class, 1);
+        assertNotNull(c);
+
+        session.close();
+    }
+
+    @Ignore("polymorphism not yet supported")
+    public void testDeletePolyOperations() {
+        CompassSession session = openSession();
+
+        A a = new A();
+        a.id = 1;
+        session.save(a);
+
+        B b = new B();
+        b.id = 2;
+        session.save(b);
+
+        a = new A();
+        a.id = 3;
+        session.save(a);
+
+        refresh(session);
+        
+        a = session.get(A.class, 1);
+        assertNotNull(a);
+
+        a = session.get(B.class, 2);
+        assertNotNull(a);
+
+        a = session.get(A.class, 3);
+        assertNotNull(a);
+
+        session.delete(A.class, 2);
+        a = session.get(B.class, 2);
+        assertNull(a);
+
+        session.delete(Contract.class, 1);
+        a = session.get(A.class, 1);
+        assertNull(a);
+
+        session.close();
+    }
+
+    @Ignore("polymorphism not yet supported")
+    public void testPolyFind() {
+        CompassSession session = openSession();
+
+        A a = new A();
+        a.id = 1;
+        a.value = "value1";
+        session.save(a);
+
+        B b = new B();
+        b.id = 2;
+        b.value = "value2";
+        session.save(b);
+
+        a = new A();
+        a.id = 3;
+        a.value = "value3";
+        session.save(a);
+
+        refresh(session);
+        
+        // search exact ones
+        assertEquals(1, session.find("A.value:value1").length());
+        assertEquals(1, session.find("B.value:value2").length());
+
+        // search poly ones based on A
+        assertEquals(1, session.find("A.value:value2").length());
+
+        // search poly ones based on the Contract
+        assertEquals(1, session.find("Contract.value:value1").length());
+        assertEquals(1, session.find("Contract.value:value2").length());
+        assertEquals(1, session.find("Contract.value:value3").length());
+
+        session.close();
+    }
+}
