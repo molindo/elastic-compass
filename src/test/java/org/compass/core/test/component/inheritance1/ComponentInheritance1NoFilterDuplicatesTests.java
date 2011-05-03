b9da13972b9ffc1e4841f72f429333d5d952b5d8
Stefan Fussenegger <stf+git@molindo.at>
2011-05-03 15:03:17 +0200

    added ComponentInheritance1Tests,NoUnmarshallInheritance1Tests, ComponentInheritance1NoFilterDuplicatesTests

---
+++ src/test/java/org/compass/core/test/component/inheritance1/ComponentInheritance1NoFilterDuplicatesTests.java
@@ -0,0 +1,54 @@
+package org.compass.core.test.component.inheritance1;
+
+import org.compass.core.CompassSession;
+import org.compass.core.config.CompassEnvironment;
+import org.compass.core.config.CompassSettings;
+import org.compass.core.test.AbstractTestCase;
+
+/**
+ * This holds a pretty complex relationship that has been reported as
+ * broken in Compass. The main goal here is to test that the relationship
+ * is supported.
+ *
+ * @author kimchy
+ */
+public class ComponentInheritance1NoFilterDuplicatesTests extends AbstractTestCase {
+
+
+    protected String[] getMappings() {
+        return new String[] {"component/inheritance1/Father.cpm.xml", "component/inheritance1/Child.cpm.xml"};
+    }
+
+    protected void addSettings(CompassSettings settings) {
+        settings.setBooleanSetting(CompassEnvironment.Osem.FILTER_DUPLICATES, false);
+    }
+
+    public void testCorrectFatherSave() throws Exception {
+        CompassSession session = openSession();
+
+        FatherImpl.resetId();
+        ChildImpl.resetId();
+
+        FatherImpl father = new FatherImpl("Sir Ivan");
+        FavouriteSonImpl favouriteSon = new FavouriteSonImpl("Ivan Jr", father);
+        father.setFavouriteSon(favouriteSon);
+
+        DaughterImpl daughter = new DaughterImpl("Betty Jr", father);
+        father.getChildren().add(daughter);
+
+        session.save(father);
+
+        refresh(session);
+        
+        father = (FatherImpl) session.load("father", father.getId());
+        assertEquals("Sir Ivan", father.getName());
+        assertNotNull(father.getFavouriteSon());
+        assertEquals("Ivan Jr", father.getFavouriteSon().getName());
+        assertSame(father, father.getFavouriteSon().getFather());
+        assertEquals(1, father.getChildren().size());
+        assertEquals("Betty Jr", ((DaughterImpl)father.getChildren().iterator().next()).getName());
+        assertSame(father, ((DaughterImpl)father.getChildren().iterator().next()).getFather());
+
+        session.close();
+    }
+}
