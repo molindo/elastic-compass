ad40606bcac986d4c0301e1000e5057848f3782a
Stefan Fussenegger <stf+git@molindo.at>
2011-05-03 16:33:55 +0200

    added cache tests, green lights

---
+++ src/test/java/org/compass/core/test/cache/AsyncInvalidationCacheTests.java
@@ -0,0 +1,16 @@
+package org.compass.core.test.cache;
+
+import org.compass.core.config.CompassSettings;
+import org.compass.core.lucene.LuceneEnvironment;
+
+/**
+ * @author kimchy
+ */
+@SuppressWarnings("deprecation")
+public class AsyncInvalidationCacheTests extends AbstractCacheTests {
+
+	protected void addSettings(CompassSettings settings) {
+        super.addSettings(settings);
+        settings.setBooleanSetting(LuceneEnvironment.SearchEngineIndex.CACHE_ASYNC_INVALIDATION, true);
+    }
+}
