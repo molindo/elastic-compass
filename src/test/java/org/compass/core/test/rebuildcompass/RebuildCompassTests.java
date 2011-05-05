75e5a8274f1a8b15df452c0a741411582e19d8a4
Stefan Fussenegger <stf+git@molindo.at>
2011-05-05 14:42:01 +0200

    RebuildCompassTests, green lights

---
+++ src/test/java/org/compass/core/test/rebuildcompass/RebuildCompassTests.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2004-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.compass.core.test.rebuildcompass;
+
+import org.compass.core.CompassSession;
+import org.compass.core.config.CompassEnvironment;
+import org.compass.core.config.CompassSettings;
+import org.compass.core.test.AbstractTestCase;
+
+/**
+ * @author kimchy
+ */
+public class RebuildCompassTests extends AbstractTestCase {
+
+    protected String[] getMappings() {
+        return new String[]{"rebuildcompass/A.cpm.xml"};
+    }
+
+    protected void addSettings(CompassSettings settings) {
+        settings.setLongSetting(CompassEnvironment.Rebuild.SLEEP_BEFORE_CLOSE, 100);
+    }
+
+    public void testRebuildCompass() throws Exception {
+        CompassSession session = openSession();
+        A a = new A();
+        a.id = 1;
+        a.value = "avalue";
+        session.save("a", a);
+        session.close();
+
+        session = openSession();
+        assertNotNull(session.get("a", 1));
+        session.close();
+
+        getCompass().getConfig().addClass(B.class);
+        getCompass().rebuild();
+
+        session = openSession();
+        B b = new B();
+        b.id = 1;
+        b.value = "bvalue";
+        session.save("b", b);
+        session.close();
+
+        session = openSession();
+        assertNotNull(session.get("a", 1));
+        assertNotNull(session.get("b", 1));
+        session.close();
+
+        getCompass().getConfig().removeMappingByClass(A.class);
+        getCompass().rebuild();
+
+        session = openSession();
+        assertNull(session.get("a", 1));
+        assertNotNull(session.get("b", 1));
+        session.close();
+
+        Thread.sleep(200);
+    }
+}
