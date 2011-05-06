28bfe0042c5b1b740a377876cfcec4b9d42b102b
Stefan Fussenegger <stf+git@molindo.at>
2011-05-06 10:32:05 +0200

    added LuceneTransactionProcessorCyclicTests - not sure what it does or tests, but it works

---
+++ src/test/java/org/compass/core/test/transaction/processor/lucene/cyclic/LuceneTransactionProcessorCyclicTests.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2004-2009 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.compass.core.test.transaction.processor.lucene.cyclic;
+
+import org.compass.core.CompassHits;
+import org.compass.core.CompassSession;
+import org.compass.core.config.CompassSettings;
+import org.compass.core.lucene.LuceneEnvironment;
+import org.compass.core.spi.InternalCompassSession;
+import org.compass.core.spi.InternalResource;
+import org.compass.core.test.AbstractTestCase;
+
+/**
+ * @author kimchy
+ */
+@SuppressWarnings("deprecation")
+public class LuceneTransactionProcessorCyclicTests extends AbstractTestCase {
+
+    protected String[] getMappings() {
+        return new String[]{"transaction/processor/lucene/cyclic//mapping.cpm.xml"};
+    }
+
+    protected void addSettings(CompassSettings settings) {
+        super.addSettings(settings);
+        settings.setSetting(LuceneEnvironment.Transaction.Processor.TYPE, LuceneEnvironment.Transaction.Processor.Lucene.NAME);
+    }
+
+    public void testBatch() {
+        CompassSession session = openSession();
+
+        Long id = new Long(1);
+        Cyclic1 cyclic1 = new Cyclic1();
+        cyclic1.setId(id);
+        cyclic1.setValue("cyclic1");
+
+        Cyclic2 cyclic2 = new Cyclic2();
+        cyclic2.setId(id);
+        cyclic2.setValue("cyclic2");
+
+        cyclic1.setCyclic2(cyclic2);
+        cyclic2.setCyclic1(cyclic1);
+
+        session.create(cyclic2);
+        session.create(cyclic1);
+
+        // verify they are not in the first level cache
+        InternalResource key = (InternalResource) getResourceFactory().createResource("cyclic1");
+        key.addProperty("id", id);
+        Object val = ((InternalCompassSession) session).getFirstLevelCache().get(key.getResourceKey());
+        assertNull(val);
+        key = (InternalResource) getResourceFactory().createResource("cyclic2");
+        key.addProperty("id", id);
+        val = ((InternalCompassSession) session).getFirstLevelCache().get(key.getResourceKey());
+        assertNull(val);
+
+        CompassHits hits = session.find("value:cyclic1");
+        assertEquals(1, hits.length());
+        hits = session.find("value:cyclic2");
+        assertEquals(1, hits.length());
+
+        cyclic2.setValue("updated");
+        session.save(cyclic2);
+
+        hits = session.find("value:cyclic2");
+        assertEquals(0, hits.length());
+        hits = session.find("value:updated");
+        assertEquals(1, hits.length());
+
+        cyclic2 = session.load(Cyclic2.class, 1);
+        assertNotNull(cyclic2);
+
+        session.delete(cyclic2);
+
+        cyclic2 = session.get(Cyclic2.class, 1);
+        assertNull(cyclic2);
+    }
+}
